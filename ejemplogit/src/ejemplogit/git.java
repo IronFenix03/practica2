092448e3fd7bf2ed2922b79a2cfff303a3c6ff1d
samu <samu@gmail.com>
2024-04-05 11:01:43 +0200

    Segunda version

--- ejemplogit/src/ejemplogit/git.java
+++ ejemplogit/src/ejemplogit/git.java
@@ -5,6 +5,7 @@ public class git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba git");
+		System.out.println("Jesusito santo");
 	}
 
 }
